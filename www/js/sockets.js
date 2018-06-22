0058493642a3b9aac8ab327e5d7ce9d8d7561adb
Jasper Vriends - V17 Development <jasper@v17.nl>
2018-06-22 11:05:03 +0200

    Removed start

--- www/js/sockets.js
+++ www/js/sockets.js
@@ -3,12 +3,6 @@
 var joined = false;
 var socket = io();
 
-
-function start()
-{
-    socket.emit("admin-dashboard-start-round");
-}
-
 socket.on('start', playScreen.start)
 socket.on('show-result', playScreen.showAnswer)
 
