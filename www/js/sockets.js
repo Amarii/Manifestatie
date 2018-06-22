b8d49e3faa6108fdf6fe91ad563194a215322b66
Jasper Vriends - V17 Development <jasper@v17.nl>
2018-06-22 12:41:53 +0200

    Disconnected = reload

--- www/js/sockets.js
+++ www/js/sockets.js
@@ -6,7 +6,7 @@ var socket = io();
 socket.on('start', playScreen.start)
 socket.on('show-result', playScreen.showAnswer)
 
-socket.on('disconnect', function() {
+socket.on('disconnected', function() {
     document.location.reload();
 });
 
